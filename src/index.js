4b4519df5eccd1f2022c08747ef15fa27ac80763
Chris Sakai <cs.chris.sakai@gmail.com>
2019-09-04 17:28:33 -0400

    The bug can be triggered even with just positive lookbehind and a named capture group

--- src/index.js
+++ src/index.js
@@ -4,11 +4,24 @@
 // // Named capture group only
 // const resourceIdPathPattern = /\/:(?<key>[a-zA-Z]+?Id|id)/
 
+// // Positive lookahead only
+// const resourceIdPathPattern = /:([a-zA-Z]+?Id(?=\/)|id)/
+
+// // Positive lookbehind only
+// const resourceIdPathPattern = /(?<=\/):([a-zA-Z]+?Id|id)/
+
 // // Positive lookahead/lookbehind only
 // const resourceIdPathPattern = /(?<=\/):([a-zA-Z]+?Id(?=\/)|id)/
 
+// Both named capture group and positive lookahead only
+// const resourceIdPathPattern = /:(?<key>[a-zA-Z]+?Id(?=\/)|id)/
+
 // All of the above regexes can be transpiled with babel just fine,
-// but when both the named capture group and positive lookahead/lookbehind
+// but when both the named capture group and positive lookbehind
 // are present, babel throws a StackOverflow error
-// Both named capture group and positive lookahead/lookbehind
-const resourceIdPathPattern = /(?<=\/):(?<key>[a-zA-Z]+?Id(?=\/)|id)/
+
+// Both named capture group and Positive lookbehind only
+const resourceIdPathPattern = /(?<=\/):(?<key>[a-zA-Z]+?Id|id)/
+
+// // Both named capture group and positive lookahead/lookbehind
+// const resourceIdPathPattern = /(?<=\/):(?<key>[a-zA-Z]+?Id|id)/
